cecf45c758bdbb6dd0ec0519ab30449ebf81947b
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-05-10 02:55:50 +0300

    Add testing to the user and to the jogging times

---
+++ spec/models/jogging_time_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe JoggingTime, type: :model do
+
+    before do
+        @user = User.create( email: 'user@example.com', password: 'password', role: 'user' )
+    end
+
+    it "is valid with valid attributes" do
+        jogging_record = @user.jogging_times.create(
+        date: Date.today,
+        distance: 10,
+        time: 60,
+        )
+        expect(jogging_record).to be_valid
+    end
+
+end
