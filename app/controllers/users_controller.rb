f4bd9687f155fb7a7974e07a8c166d581eb35878
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-05-09 15:47:39 +0300

    Add the average speed & distance per week method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,17 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
     load_and_authorize_resource
 
+
+
+    
+    
+    
+    
     def index
         @users = User.all
         render json: @users
     end
+    
     def create
         @user = User.new(user_params)
         if @user.save
@@ -30,7 +37,7 @@ class UsersController < ApplicationController
             render json: @user.errors, status: :unprocessable_entity
         end
     end
-
+    
     def destroy
         @user = User.find(params[:id])
         if @user.destroy
@@ -40,8 +47,51 @@ class UsersController < ApplicationController
         end
     end
 
-    private
+    def weekly_report
+        # Find the user
+        user = User.find(params[:id])
+      
+        # Group jogging times by week
+        jogging_times_by_week = user.jogging_times.group_by { |jt| jt.date.beginning_of_week }
+      
+        # Calculate average distance and speed each week
+        weekly_averages = jogging_times_by_week.transform_values do |jogging_times|
+          distances = jogging_times.pluck(:distance).map(&:to_f)
+          total_distance = distances.sum
+          total_time = jogging_times.sum { |jt| jt.time.to_i }.to_i
+          average_speed = total_distance / (total_time / 3600.0) # Speed in km/h
+          {
+            distance: (total_distance / distances.size).round(2), # Average distance in km
+            speed: average_speed.round(2) # Average speed in km/h
+          }
+        end
+      
+        # Render the report as JSON
+        render json: {
+          user: user.email,
+          weekly_averages: weekly_averages
+        }
+      end
+      
+    
+    
+    
+    def filter_jogging_times_by_dates
+        @user = User.find(params[:id])
+        from_date = params[:from_date]
+        to_date = params[:to_date]
+        @jogging_times = @user.jogging_times.where(date: from_date..to_date)
+        render json: {
+          @user => {
+            email: @user.email,
+            role: @user.role,
+            jogging_times: @jogging_times
+          }
+        }
+    end
 
+    private
+    
     def user_params
         params.require(:user).permit(:email, :password, :role)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  # =======================================================================================================
+  
   get 'user_admin/create'
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
@@ -9,9 +12,12 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  
+  # =======================================================================================================
 
   get 'current_user', to: 'current_user#index'
 
+  # =======================================================================================================
   
   # admin routes
   post 'jogging_times/add-jogging-times-to-user', to: 'jogging_times#add_records_to_user_by_admin'
@@ -19,13 +25,25 @@ Rails.application.routes.draw do
   post 'jogging_times/update-jogging-times-to-user', to: 'jogging_times#update_a_user_records_by_admin'
   get 'jogging_times/show-jogging-times-to-user/:id', to: 'jogging_times#show_user_records_by_admin'
   resources :jogging_times
+  
+  # =======================================================================================================
+
   # why do I need this?
   # the admin can simply signup as a normal user and that will create a user
   # this can be easily handled by the frontend
   #  |
   #  V
   # post 'users/create', to: 'users#create'
+  
+  # =======================================================================================================
+  
   put 'users/update/:id', to: 'users#update'
   delete 'users/destroy/:id', to: 'users#destroy'
   get 'users/show/:id', to: 'users#show'
+  
+  # =======================================================================================================
+
+  get 'users/filter_jogging_times_by_dates', to: 'users#filter_jogging_times_by_dates'
+  get 'users/weekly_report/:id', to: 'users#weekly_report'
+  
 end
