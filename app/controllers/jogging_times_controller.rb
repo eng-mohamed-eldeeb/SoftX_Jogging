b4bd89ed6f4194dd31a5ad0ad7e36f5fa2d13677
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-05-10 14:20:55 +0300

    Create the documentation

--- app/controllers/jogging_times_controller.rb
+++ app/controllers/jogging_times_controller.rb
@@ -41,65 +41,45 @@ class JoggingTimesController < ApplicationController
 
   # jogging_times/add-jogging-times-to-user
   def add_records_to_user_by_admin
-    if current_user.admin?
-      @jogging_time = JoggingTime.new(jogging_time_add_records_to_user_by_admin_params)
-      if @jogging_time.save
-        render json: @jogging_time, status: :created
-      else
-        render json: @jogging_time.errors, status: :unprocessable_entity
-      end
+    @jogging_time = JoggingTime.new(jogging_time_add_records_to_user_by_admin_params)
+    if @jogging_time.save
+      render json: @jogging_time, status: :created
     else
-      render json: { error: 'You are not admin' }, status: :unprocessable_entity
+      render json: @jogging_time.errors, status: :unprocessable_entity
     end
   end
 
   # jogging_times/remove-jogging-times-to-user
   def remove_records_from_user_by_admin
-    if current_user.admin?
-      @jogging_time = JoggingTime.find(jogging_time_remove_records_to_user_by_admin_params[:jogging_time_id])
-      @jogging_time.destroy
-      head :no_content
-    else
-      render json: { error: 'You are not admin' }, status: :unprocessable_entity
-    end
+    @jogging_time = JoggingTime.find(params[:id])
+    @jogging_time.destroy
+    head :no_content
   end
 
   # jogging_times/update-jogging-times-to-user
   def update_a_user_records_by_admin
-    if current_user.admin?
-      @jogging_time = JoggingTime.find(jogging_time_update_records_to_user_by_admin_params[:jogging_time_id])
-      if @jogging_time.update(jogging_time_add_records_to_user_by_admin_params)
-        render json: @jogging_time, status: :ok
-      else
-        render json: @jogging_time.errors, status: :unprocessable_entity
-      end
+    @jogging_time = JoggingTime.find(params[:id])
+    if @jogging_time.update(jogging_time_add_records_to_user_by_admin_params)
+      render json: @jogging_time, status: :ok
     else
-      render json: { error: 'You are not admin' }, status: :unprocessable_entity
+      render json: @jogging_time.errors, status: :unprocessable_entity
     end
   end
 
   # jogging_times/show-jogging-times-to-user
   def show_user_records_by_admin
-    if current_user.admin?
-      @jogging_times = JoggingTime.where(user_id: params[:id])
-      render json: @jogging_times
-    else
-      render json: { error: 'You are not admin' }, status: :unprocessable_entity
-    end
+    jogging_times = User.find(params[:user_id]).jogging_times.all
+    render json: jogging_times
   end
 
   private
-
-  def show_user_records_by_admin_params
-    params.require(:jogging_time).permit(:id)
-  end
 
   def jogging_time_add_records_to_user_by_admin_params
     params.require(:jogging_time).permit(:distance, :time, :date, :user_id)
   end
 
   def jogging_time_update_records_to_user_by_admin_params
-    params.require(:jogging_time).permit(:distance, :time, :date, :user_id, :jogging_time_id)
+    params.require(:jogging_time).permit(:distance, :time, :date, :user_id)
   end
 
   def jogging_time_remove_records_to_user_by_admin_params
